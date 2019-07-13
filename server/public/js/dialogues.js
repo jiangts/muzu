51a0a14dcc1f6ec2c893e4b80cd58a3f94cdae2c
Stephanie Zhang <zhang.steph@gmail.com>
2019-07-13 14:43:14 -0700

    with gifs

--- server/public/js/dialogues.js
+++ server/public/js/dialogues.js
@@ -1,309 +1,404 @@
-const start_state = 'intro';
+const start_state = "intro";
 
 const states = [
   {
-    name: 'intro'
+    name: "intro"
   },
   {
-    name: 'stuck'
+    name: "stuck"
   },
   {
-    name: 'challenged'
+    name: "challenged"
   },
   {
-    name: 'bored'
+    name: "bored"
   },
   {
-    name: 'unstuck'
+    name: "unstuck"
   }
 ];
 
 const transitions = [
   {
-    name: 'figured_out'
+    name: "figured_out"
   },
   {
-    name: 'got_confused'
+    name: "got_confused"
   }
-]
+];
 
-let result = '<p>Make your project talk to you! Add extra "say" blocks in-between the blocks you\'re testing, to see if all of your blocks are being run in your project, or to check a particular variable.</p>';
-
-
+let result0 = `<p>
+  <img src="images/say_block.gif" class="some-img" />
+  Pull apart your blocks and click on each one.
+  Does each block do what you expect?
+  Now try attaching some blocks back together and clicking on the chunk.
+  Does it do what you expect?
+  </p><p>
+<img src="images/1block_description.gif" class="some-img" /></p>
+  `;
+let oneBlock_description = `<p>
+    Pull apart your blocks and click on each one.
+    Does each block do what you expect?
+    Now try attaching some blocks back together and clicking on the chunk.
+    Does it do what you expect?
+    </p><p>
+  <img src="images/1block_description.gif" class="some-img" /></p>
+    `;
+let projectTalk_description = `<p>
+Make your project talk to you!
+Add extra "say" blocks in-between the blocks you\'re testing,
+to see if all of your blocks are being run in your project,
+or to check a particular variable.</p>
+<p>
+<img src="images/say_blocks.png" class="some-img" />
+</p>
+<p>Example 1: Check to see which blocks of code are running</p>
+<p>
+<img src="images/say_block_description1.gif" class="some-img" />
+</p>
+<p>Example 2: Check to see the value of certain variables (like x position) and if certain blocks are running (like block inside an "if" block)</p>
+<p>
+<img src="images/say_block_description2.gif" class="some-img" />
+</p>
+`;
+let projectTalk_tutorial = `
+<p>
+1) Click on the "Looks" category of code blocks on the left side of your window (hint: it's purple!)
+</p><p>
+<img src="images/say_block_tutorial1.gif" class="some-img" />
+</p><p>
+2) Add a "say [Hello!] for [2] seconds" block into areas in your code where you want to check if your character has done something, or to check if your code has gotten to a particular block. For example, let's say you want to check if your character has moved 10 steps. In this case, put your "say" block after the "move 10 steps" block.
+</p><p>
+<img src="images/say_block_tutorial2.gif" class="some-img" />
+</p><p>
+3) Change the "Hello!" part of the say block to something that is informative of what your character is doing. For example, this could be "I've moved 10 steps!"
+</p><p>
+<img src="images/say_block_tutorial3.gif" class="some-img" />
+</p>
+<p>
+4) Now, run your code with the extra say blocks! Use the "say" blocks to check what your character is doing, or to check if it has gotten to a particular part of your code (like an "if" block)
+</p><p>
+<img src="images/say_block_tutorial4.gif" class="some-img" />
+</p>
+`;
+let searchOnline_description = `<p>Try searching online to see if you can find the answer! In the search bar, type: "scratch code" and whatever question you have.</p>
+<p><img src="images/search_online_description1.gif" class="some-img" /></p>`;
 
 const toolkit = [
   {
-    title: 'A block at a time',
+    title: "A block at a time",
     tabs: [
       {
-        title: 'description',
-        markdown: result
+        title: "description",
+        markdown: oneBlock_description
       },
       {
-        title: 'tutorial',
-        markdown: result
+        title: "tutorial",
+        markdown: result0
       },
       {
-        title: 'workspace',
-        markdown: result
+        title: "workspace",
+        markdown: result0
       }
     ]
   },
   {
-    title: 'Project talk',
+    title: "Project talk",
     tabs: [
       {
-        title: 'description',
-        markdown: result
+        title: "description",
+        markdown: projectTalk_description
       },
       {
-        title: 'tutorial',
-        markdown: result
+        title: "tutorial",
+        markdown: projectTalk_tutorial
       },
       {
-        title: 'workspace',
-        markdown: result
+        title: "workspace",
+        markdown: projectTalk_description
       }
     ]
   },
   {
-    title: 'Search Online',
+    title: "Search Online",
     tabs: [
       {
-        title: 'description',
-        markdown: result
+        title: "description",
+        markdown: searchOnline_description
       },
       {
-        title: 'tutorial',
-        markdown: result
+        title: "tutorial",
+        markdown: result0
       },
       {
-        title: 'workspace',
-        markdown: result
+        title: "workspace",
+        markdown: result0
       }
     ]
   },
   {
-    title: 'Take a break',
+    title: "Take a break",
     tabs: [
       {
-        title: 'description',
-        markdown: result
+        title: "description",
+        markdown: result0
       },
       {
-        title: 'tutorial',
-        markdown: result
+        title: "tutorial",
+        markdown: result0
       },
       {
-        title: 'workspace',
-        markdown: result
+        title: "workspace",
+        markdown: result0
       }
     ]
   }
-]
-
+];
 
 const dialogues = [
   {
-    type: 'intro',
+    type: "intro",
     dialogue: [
       {
-        speaker: 'bot',
+        speaker: "bot",
+        content: `![muzu gif](images/muzu128.png)`
+      },
+      {
+        speaker: "bot",
         content: `Hi! My name is Muzu, and I am here to support you as you program amazing things! What is your name?`
       },
       {
-        speaker: 'human',
+        speaker: "human",
         input: {
-          variable: 'name',
-          placeholder: 'Your name'
+          variable: "name",
+          placeholder: "Your name"
         }
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Nice to meet you {{=name}}! I'm here to help you if you get stuck.`,
-        NEXT: 'main_loop'
+        NEXT: "main_loop"
       }
     ]
   },
   {
-    type: 'main_loop',
+    type: "main_loop",
     dialogue: [
       {
-        speaker: 'bot',
-        variable: 'NEXT',
+        speaker: "bot",
+        variable: "NEXT",
         buttons: [
           {
             text: `I got stuck :(`,
-            value: 'stuck'
+            value: "stuck"
           },
           {
             text: `This is fun!`,
-            value: 'intro2'
+            value: "fun"
           }
         ]
       }
     ]
   },
   {
-    type: 'stuck',
+    type: "stuck",
     dialogue: [
       {
-        speaker: 'bot',
-        content: `Hi {{=name}}, let's get unstuck together!`
+        speaker: "bot",
+        content: `![muzu brain](images/sad_muzu.png) I'm sorry to hear that {{=name}}`
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
+        content: `But no worries! When you work hard to get unstuck, your brain is growing! ![muzu brain](images/brain_muzu.png)`
+      },
+      {
+        speaker: "bot",
+        content: `Let's get unstuck together!`
+      },
+      {
+        speaker: "bot",
         content: `Can you explain your problem to me in words?`
       },
       {
-        speaker: 'human',
+        speaker: "human",
         input: {
-          variable: 'bug_description',
-          placeholder: 'Describe your bug'
+          variable: "bug_description",
+          placeholder: "Describe your bug"
         }
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Let's go to the debugging toolbox!`,
-        NEXT: 'toolbox'
+        NEXT: "toolbox"
+      },
+      {
+        speaker: "bot",
+        variable: "NEXT",
+        buttons: [
+          {
+            text: `I'm still' stuck :(`,
+            value: "stuck"
+          },
+          {
+            text: `I figured out my bug!`,
+            value: "solution"
+          }
+        ]
       }
     ]
   },
   {
-    type: 'solution',
+    type: "solution",
     dialogue: [
       {
-        speaker: 'bot',
+        speaker: "bot",
+        content: `![muzu brain](images/cheerful_muzu.png)`
+      },
+      {
+        speaker: "bot",
         content: `Great work debugging, {{=name}}! It looks like the problem was that, "{{=reason}}".`
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Now can you use words to state what a solution might be?`
       },
       {
-        speaker: 'human',
+        speaker: "human",
         input: {
-          variable: 'soln_description',
-          placeholder: 'Describe a solution'
-        },
+          variable: "soln_description",
+          placeholder: "Describe a solution"
+        }
       },
       {
-        speaker: 'bot',
-        content: 'Great work debugging, {{=name}}!',
-        NEXT: 'main_loop'
+        speaker: "bot",
+        content: "Great work debugging, {{=name}}!",
+        NEXT: "main_loop"
       }
     ]
   },
   {
-    type: 'toolbox',
+    type: "toolbox",
+    dialogue: []
+  },
+  {
+    type: "fun",
     dialogue: [
-
+      {
+        speaker: "bot",
+        content:
+          "glad to hear it {{=name}}! As you work hard, your brain is growing strong!"
+      },
+      {
+        speaker: "bot",
+        content: "I'll be here if you need any help :)",
+        NEXT: "main_loop"
+      }
     ]
   },
   {
-    type: 'intro2',
+    type: "intro2",
     dialogue: [
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Hi! My name is Muzu, and I am here to support you as you program amazing things! What is your name?`
       },
       {
-        speaker: 'human',
+        speaker: "human",
         input: {
-          variable: 'name',
-          placeholder: 'Your name'
+          variable: "name",
+          placeholder: "Your name"
         }
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Nice to meet you {{=name}}! What are you hoping to create in Scratch today?`
       },
       {
-        speaker: 'bot',
-        variable: 'project',
+        speaker: "bot",
+        variable: "project",
         buttons: [
           {
             text: `Story`,
-            value: 'story'
+            value: "story"
           },
           {
             text: `Game`,
-            value: 'game'
+            value: "game"
           },
           {
             text: `Animation`,
-            value: 'animation'
+            value: "animation"
           },
           {
             text: `Other`,
-            value: 'other',
-            type: 'input'
+            value: "other",
+            type: "input"
           }
         ]
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `I don't know how to program myself, but am here to support you as you create your {{=project}}! Feel free to click on me whenever you want to chat. I will also check in on you from time to time. Happy programming!`
       },
       {
-        speaker: 'bot',
-        variable: 'feeling',
+        speaker: "bot",
+        variable: "feeling",
         buttons: [
           {
-            text: 'Excited!',
-            value: 'excited'
+            text: "Excited!",
+            value: "excited"
           },
           {
-            text: 'Confused',
-            value: 'confused'
+            text: "Confused",
+            value: "confused"
           },
           {
-            text: 'Stuck',
-            value: 'stuck'
+            text: "Stuck",
+            value: "stuck"
           },
           {
-            text: 'Unstuck',
-            value: 'unstuck'
+            text: "Unstuck",
+            value: "unstuck"
           }
         ]
       }
     ]
   },
   {
-    type: 'confused',
+    type: "confused",
     dialogue: [
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Sorry to hear that you're confused. But when you're confused, that's when you can learn the most! What are you confused about?`
       },
       {
-        speaker: 'human',
+        speaker: "human",
         input: {
-          variable: 'current_project'
+          variable: "current_project"
         }
       },
       {
-        speaker: 'bot',
+        speaker: "bot",
         content: `Yes, that does sound challenging. Hm... maybe you can try some of these tips!`
       },
       {
-        speaker: 'bot',
-        variable: 'topic',
+        speaker: "bot",
+        variable: "topic",
         buttons: [
           {
             text: `Ask for help (maybe from a teacher or friend)`,
-            value: 'ask_help'
+            value: "ask_help"
           },
           {
             text: `Look up examples of other {{=current_project}}`,
-            value: 'view_example'
+            value: "view_example"
           },
           {
             text: `Click through some tutorials on {{=current_project}}`,
-            value: 'watch_tutorial'
-          },
+            value: "watch_tutorial"
+          }
         ]
       }
     ]
   }
-]
+];
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -3,16 +3,16 @@ var get_tmpl = (function() {
   return function(url) {
     return new Promise(function(fulfill, reject) {
       if (tmpls[url]) {
-        fulfill(tmpls[url])
+        fulfill(tmpls[url]);
       } else {
-        $.get(url, (tmpl) => {
+        $.get(url, tmpl => {
           tmpls[url] = new t(tmpl);
-          fulfill(tmpls[url])
+          fulfill(tmpls[url]);
         });
       }
-    })
-  }
-})()
+    });
+  };
+})();
 
 var load_tmpl = (function() {
   var tmpls = {};
@@ -20,51 +20,47 @@ var load_tmpl = (function() {
     var load = function() {
       var template = new t(tmpls[url]);
       $(selector).html(template.render(data));
-    }
+    };
 
     if (tmpls[url]) {
-      load()
+      load();
     } else {
-      $.get(url, (tmpl) => {
+      $.get(url, tmpl => {
         tmpls[url] = tmpl;
-        load()
+        load();
       });
     }
-  }
-})()
-
-(function(jQuery) {
-
-  jQuery.eventEmitter = {
-    _JQInit: function() {
-      this._JQ = jQuery(this);
-    },
-    emit: function(evt, data) {
-      !this._JQ && this._JQInit();
-      this._JQ.trigger(evt, data);
-    },
-    once: function(evt, handler) {
-      !this._JQ && this._JQInit();
-      this._JQ.one(evt, handler);
-    },
-    on: function(evt, handler) {
-      !this._JQ && this._JQInit();
-      this._JQ.bind(evt, handler);
-    },
-    off: function(evt, handler) {
-      !this._JQ && this._JQInit();
-      this._JQ.unbind(evt, handler);
-    }
   };
-
-}(jQuery));
-
+})()(
+  (function(jQuery) {
+    jQuery.eventEmitter = {
+      _JQInit: function() {
+        this._JQ = jQuery(this);
+      },
+      emit: function(evt, data) {
+        !this._JQ && this._JQInit();
+        this._JQ.trigger(evt, data);
+      },
+      once: function(evt, handler) {
+        !this._JQ && this._JQInit();
+        this._JQ.one(evt, handler);
+      },
+      on: function(evt, handler) {
+        !this._JQ && this._JQInit();
+        this._JQ.bind(evt, handler);
+      },
+      off: function(evt, handler) {
+        !this._JQ && this._JQInit();
+        this._JQ.unbind(evt, handler);
+      }
+    };
+  })(jQuery)
+);
 
 $(document).ready(function() {
+  $(".menu .item").tab();
 
-  $('.menu .item').tab();
-
-  var widget_id = 'muzu-chatbot';
+  var widget_id = "muzu-chatbot";
 
   var botui = new BotUI(widget_id);
 
@@ -85,119 +81,135 @@ $(document).ready(function() {
   var chat_context = {};
 
   Promise.all([
-    get_tmpl('tmpls/toolbox.html'),
-    get_tmpl('tmpls/toolkit.html'),
+    get_tmpl("tmpls/toolbox.html"),
+    get_tmpl("tmpls/toolkit.html")
   ]).then(tmpls => {
-    var [
-      toolbox_tmpl,
-      toolkit_tmpl
-    ] = tmpls;
+    var [toolbox_tmpl, toolkit_tmpl] = tmpls;
 
-    var bug = '';
+    var bug = "";
     var reasons = [];
 
     var render_bug;
     var render_reason;
 
     render_bug = function(reasons) {
-      $('#toolbox-container').html(toolbox_tmpl.render({
-        page0: true,
-        bug: bug,
-        reasons: reasons.map((r, i) => {
-          r.nu = i + 1;
-          if (r.culprit === true) {
-            r.color = 'green'
-          } else if (r.culprit === false) {
-            r.color = 'red'
-          } else {
-            r.color = 'grey'
-          }
-          return r
+      $("#toolbox-container").html(
+        toolbox_tmpl.render({
+          page0: true,
+          bug: bug,
+          reasons: reasons.map((r, i) => {
+            r.nu = i + 1;
+            if (r.culprit === true) {
+              r.color = "green";
+            } else if (r.culprit === false) {
+              r.color = "red";
+            } else {
+              r.color = "grey";
+            }
+            return r;
+          })
         })
-      }));
+      );
 
-      $('.muzu-reason').click(function(el) {
-        var reason_index = $(this).attr('data-reason');
+      $(".muzu-reason").click(function(el) {
+        var reason_index = $(this).attr("data-reason");
 
         render_reason(reason_index);
       });
 
-      $('#add-reason').click(function(el) {
-        var new_reason = $(this).prev().val().trim()
+      $("#add-reason").click(function(el) {
+        var new_reason = $(this)
+          .prev()
+          .val()
+          .trim();
         if (new_reason.length) {
           reasons.push({
             text: new_reason
-          })
+          });
           render_bug(reasons);
         }
       });
-    }
+    };
 
     render_reason = function(reason_index) {
-
-      $('#toolbox-container').html(toolbox_tmpl.render({
-        page1: true,
-        reason_index: reason_index,
-        reason: reasons[reason_index],
-        toolkit: toolkit
-      }));
-
-      $('#triage-reason .close').parent().click(function() {
-        reasons[reason_index].culprit = false;
-        render_bug(reasons)
-      })
-
-      $('#triage-reason .check').parent().click(function() {
-        reasons[reason_index].culprit = true;
-        render_bug(reasons)
-        setTimeout(function() {
-          $('[data-tab="muzu"]').click();
-          run_dialogue(botui, dialogues, 'solution', Object.assign(chat_context, {
-            reason: reasons[reason_index].text
-          }));
-        }, 2000)
-      })
-
-
-      $('#muzu-toolkit').html(toolkit_tmpl.render({
-        page1: true,
-        reason_index: reason_index,
-        reason: reasons[reason_index],
-        toolkit: toolkit
-      }));
-
-      $('.muzu-tool').click(function(el) {
-        var tool_index = $(this).attr('data-tool');
-
-        $('#muzu-toolkit').html(toolkit_tmpl.render({
+      $("#toolbox-container").html(
+        toolbox_tmpl.render({
           page1: true,
-          page2: true,
           reason_index: reason_index,
           reason: reasons[reason_index],
-          tool: toolkit[tool_index]
-        }));
-        $('.menu .item').tab();
-        $('#muzu-toolkit .menu .item').first().click();
-        $('#muzu-toolkit .back').click(function() {
-          render_reason(reason_index)
+          toolkit: toolkit
+        })
+      );
+
+      $("#triage-reason .close")
+        .parent()
+        .click(function() {
+          reasons[reason_index].culprit = false;
+          render_bug(reasons);
+        });
+
+      $("#triage-reason .check")
+        .parent()
+        .click(function() {
+          reasons[reason_index].culprit = true;
+          render_bug(reasons);
+          setTimeout(function() {
+            $('[data-tab="muzu"]').click();
+            run_dialogue(
+              botui,
+              dialogues,
+              "solution",
+              Object.assign(chat_context, {
+                reason: reasons[reason_index].text
+              })
+            );
+          }, 2000);
+        });
+
+      $("#muzu-toolkit").html(
+        toolkit_tmpl.render({
+          page1: true,
+          reason_index: reason_index,
+          reason: reasons[reason_index],
+          toolkit: toolkit
+        })
+      );
+
+      $(".muzu-tool").click(function(el) {
+        var tool_index = $(this).attr("data-tool");
+
+        $("#muzu-toolkit").html(
+          toolkit_tmpl.render({
+            page1: true,
+            page2: true,
+            reason_index: reason_index,
+            reason: reasons[reason_index],
+            tool: toolkit[tool_index]
+          })
+        );
+        $(".menu .item").tab();
+        $("#muzu-toolkit .menu .item")
+          .first()
+          .click();
+        $("#muzu-toolkit .back").click(function() {
+          render_reason(reason_index);
         });
       });
-    }
+    };
 
     render_bug(reasons);
 
-    $.eventEmitter.on('chatbot', function(ev, data) {
-      console.log('data', data);
-      if (data.NEXT === 'toolbox') {
+    $.eventEmitter.on("chatbot", function(ev, data) {
+      console.log("data", data);
+      if (data.NEXT === "toolbox") {
         bug = data.bug_description;
         render_bug(reasons);
         setTimeout(function() {
           $('[data-tab="toolbox"]').click();
-        }, 2000)
+        }, 2000);
       }
     });
-  })
+  });
 
-  run_dialogue(botui, dialogues, 'intro', chat_context);
-
+  run_dialogue(botui, dialogues, "intro", chat_context);
 });
