2644ef1dcaa014a66c29bb64a3e867063b9329ba
Stephanie Zhang <zhang.steph@gmail.com>
2019-07-14 23:26:23 -0700

    with search online and take a break

--- server/public/js/dialogues.js
+++ server/public/js/dialogues.js
@@ -27,15 +27,27 @@ const transitions = [
   }
 ];
 
-let result0 = `<p>
-  <img src="images/say_block.gif" class="some-img" />
-  Pull apart your blocks and click on each one.
-  Does each block do what you expect?
-  Now try attaching some blocks back together and clicking on the chunk.
-  Does it do what you expect?
+let break_description = `<p>
+  Sometimes, we just need a little time to take a break from coding! Coming back might help us notice our bug. Take a 5 minute break! Walk around, do some push-ups or go to the bathroom :)
   </p><p>
-<img src="images/1block_description.gif" class="some-img" /></p>
+<iframe src="https://giphy.com/embed/muCo9BLS7vjErTON27" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>
   `;
+
+let break_tutorial = `<p>
+1) create a new tab in your browser by clicking the "+" button at the top of your window, or pressing "command" + "t". In the search bar, type in "5 minute timer"
+    </p>
+    <p><img src="images/break_tutorial1.gif" class="some-img" style="padding-top:20px; padding-bottom:20px"/></p>
+    <p>
+2) Press enter, and the timer should start counting down
+        </p>
+        <p><img src="images/break_tutorial2.gif" class="some-img" style="padding-top:20px; padding-bottom:20px"/></p>
+        <p>3) Now, step away from your computer, and take a 5 minute stretch break! </p>
+<p>4) After the timer goes off, come back to your code and take another look at it. Stepping away for a while can help you see things from a different perspective!</p>
+
+
+
+
+    `;
 let oneBlock_description = `<p>
     Pull apart your blocks and click on each one.
     Does each block do what you expect?
@@ -44,6 +56,25 @@ let oneBlock_description = `<p>
     </p><p>
   <img src="images/1block_description.gif" class="some-img" /></p>
     `;
+let oneBlock_tutorial = `<p>
+        1) Click on the stop button to make sure that none of your other code is running
+        </p>
+        <p><img src="images/1block_tutorial1.gif" class="some-img" /></p>
+        <p>2) Now, if there's a part in your code where you are confused about, drag it out of the overall code, and seperate all the blocks. For example, in the example below, the if block doesn't seem to be running. So we took apart all the pieces in the block</p>
+        <p><img src="images/1block_tutorial2.gif" class="some-img" /></p>
+        <p>
+        3) Now, click each block to see what each block does!
+        </p>
+        <p><img src="images/1block_tutorial3.gif" class="some-img" /></p>
+        <p>
+        4) Now, put some blocks back together and click the chunk to see what it does
+        </p>
+        <p><img src="images/1block_tutorial4.gif" class="some-img" /></p>
+        5) Now, put all the code back together to see what happens. Here, we found that even though all the blocks worked individually, they didn't work when we put it back into the "if" block. That's because the x position is not greater than 50, so the code inside that block never get's run!
+        </p>
+        <p><img src="images/1block_tutorial5.gif" class="some-img" /></p>
+        `;
+
 let projectTalk_description = `<p>
 Make your project talk to you!
 Add extra "say" blocks in-between the blocks you\'re testing,
@@ -52,13 +83,16 @@ or to check a particular variable.</p>
 <p>
 <img src="images/say_blocks.png" class="some-img" />
 </p>
-<p>Example 1: Check to see which blocks of code are running</p>
+<p><b>Example 1:</b> Check to see which blocks of code are running</p>
 <p>
 <img src="images/say_block_description1.gif" class="some-img" />
 </p>
-<p>Example 2: Check to see the value of certain variables (like x position) and if certain blocks are running (like block inside an "if" block)</p>
+<p><b>Example 2:</b> Check to see the value of certain variables (like x position) and if certain blocks are running (like block inside an "if" block)</p>
 <p>
 <img src="images/say_block_description2.gif" class="some-img" />
+</p>
+<p>
+<b>Tip:</b> If your code is in a "forever" block or "if" block, then you should use a "say [Hello]" block, otherwise, you should use a "say [Hello] for [2] seconds" block.
 </p>
 `;
 let projectTalk_tutorial = `
@@ -67,7 +101,7 @@ let projectTalk_tutorial = `
 </p><p>
 <img src="images/say_block_tutorial1.gif" class="some-img" />
 </p><p>
-2) Add a "say [Hello!] for [2] seconds" block into areas in your code where you want to check if your character has done something, or to check if your code has gotten to a particular block. For example, let's say you want to check if your character has moved 10 steps. In this case, put your "say" block after the "move 10 steps" block.
+2) Add a "say [Hello!] for [2] seconds" or "say [Hello!]" block into areas in your code where you want to check if your character has done something, or to check if your code has gotten to a particular part. For example, let's say you want to check when your character has moved 10 steps. In this case, put your "say" block after the "move 10 steps" block.
 </p><p>
 <img src="images/say_block_tutorial2.gif" class="some-img" />
 </p><p>
@@ -81,12 +115,39 @@ let projectTalk_tutorial = `
 <img src="images/say_block_tutorial4.gif" class="some-img" />
 </p>
 `;
-let projectTalk_workspace = `<p>Use this section to write any notes while you debug! </p><textarea rows="4" cols="75" style="outline: none;">
+let workspace = `<p>Use this section to write any notes while you debug! </p><textarea rows="4" cols="75" style="outline: none;">
 
 </textarea>`;
 
-let searchOnline_description = `<p>Try searching online to see if you can find the answer! In the search bar, type: "scratch code" and whatever question you have.</p>
+let searchOnline_description = `<p>Try searching online to see if you can find the answer! In the search bar, type: "scratch " followed by whatever question you have.</p>
 <p><img src="images/search_online_description1.gif" class="some-img" /></p>`;
+
+let searchOnline_tutorial = `<p>
+1) Create a new tab in your browser window, by either clicking the "+" button at the top of your window or pressing "command" + "t"
+</p>
+<p><img src="images/search_online_tutorial0.gif" class="some-img" style="padding-top:20px; padding-bottom:20px"/></p>
+<p>
+2) In the search bar (or URL bar) type in "scratch" followed by whatever question you have.
+</p>
+<p><img src="images/search_online_tutorial1.gif" class="some-img" style="padding-top:20px; padding-bottom:20px" /></p>
+<p>
+3) Make sure you don't use terms that are specific to your code (like cat or dinosaur) instead use more general terms (like sprite or character)
+</p>
+<p><img src="images/search_online_tutorial2.gif" class="some-img" style="padding-top:20px; padding-bottom:20px" /></p>
+<p>4) Press enter to search!
+</p>
+<p>5) When the search results come up, look for links that either say "Scratch Wiki" or "Discuss Scratch". These are usually the most helpful!
+</p>
+<p><img src="images/search_online_tutorial5b.png" class="some-img" style="max-width:50%;
+    height:auto;"/></p>
+<p><img src="images/search_online_tutorial5a.png" class="some-img" style="max-width:70%;
+    height:auto;" /></p>
+<p>6) If the search doesn't come up with anything helpful, try searching a different word that means the same thing.
+</p>
+<p><img src="images/search_online_tutorial6.gif" class="some-img style="padding-top:20px; padding-bottom:20px" /></p>
+
+
+`;
 
 const toolkit = [
   {
@@ -98,11 +159,11 @@ const toolkit = [
       },
       {
         title: "tutorial",
-        markdown: result0
+        markdown: oneBlock_tutorial
       },
       {
         title: "workspace",
-        markdown: result0
+        markdown: workspace
       }
     ]
   },
@@ -119,7 +180,7 @@ const toolkit = [
       },
       {
         title: "workspace",
-        markdown: projectTalk_workspace
+        markdown: workspace
       }
     ]
   },
@@ -132,11 +193,11 @@ const toolkit = [
       },
       {
         title: "tutorial",
-        markdown: result0
+        markdown: searchOnline_tutorial
       },
       {
         title: "workspace",
-        markdown: result0
+        markdown: workspace
       }
     ]
   },
@@ -145,15 +206,15 @@ const toolkit = [
     tabs: [
       {
         title: "description",
-        markdown: result0
+        markdown: break_description
       },
       {
         title: "tutorial",
-        markdown: result0
+        markdown: break_tutorial
       },
       {
         title: "workspace",
-        markdown: result0
+        markdown: workspace
       }
     ]
   }
@@ -209,11 +270,11 @@ const dialogues = [
     dialogue: [
       {
         speaker: "bot",
-        content: `I'm sorry to hear that {{=name}}...`
+        content: `I'm sorry to hear that {{=name}}...but it's ok to be stuck!`
       },
       {
         speaker: "bot",
-        content: `But no worries! When you work hard to get unstuck, your brain is growing!`
+        content: `Because when you work hard to get unstuck, your brain is growing!`
       },
       {
         speaker: "bot",
@@ -221,7 +282,7 @@ const dialogues = [
       },
       {
         speaker: "bot",
-        content: `Let's get unstuck together!`
+        content: `Let's try to get unstuck together!`
       },
       {
         speaker: "bot",
@@ -236,32 +297,32 @@ const dialogues = [
       },
       {
         speaker: "bot",
-        content: `Let's go to the debugging toolbox!`,
-        NEXT: "toolbox"
+        content: `Hm...that does sound challenging. Let's go to the debugging toolbox to work through it!`
       },
       {
         speaker: "bot",
         variable: "NEXT",
         buttons: [
           {
-            text: `I'm still' stuck :(`,
-            value: "stuck"
-          },
-          {
-            text: `I figured out my bug!`,
-            value: "solution"
+            text: `Let's go!`,
+            value: "toolbox"
           }
         ]
       }
     ]
   },
   {
-    type: "solution",
+    type: "toobox",
     dialogue: [
       {
         speaker: "bot",
-        content: `![muzu brain](images/cheerful_muzu.png)`
-      },
+        NEXT: "toolbox"
+      }
+    ]
+  },
+  {
+    type: "solution",
+    dialogue: [
       {
         speaker: "bot",
         content: `Great work debugging, {{=name}}! It looks like the problem was that, "{{=reason}}".`
@@ -290,7 +351,7 @@ const dialogues = [
             value: "stuck"
           },
           {
-            text: `Nope I got it!`,
+            text: `Nope, I got it!`,
             value: "no help solution"
           }
         ]
@@ -302,7 +363,35 @@ const dialogues = [
     dialogue: [
       {
         speaker: "bot",
-        content: "Great work debugging, {{=name}}!",
+        content: `![muzu brain](images/cheerful_muzu.png)`
+      },
+      {
+        speaker: "bot",
+        content:
+          "Great work debugging, {{=name}}! I know you worked really hard!"
+      },
+      {
+        speaker: "bot",
+        content: "Did you learn anything from working through that bug?",
+        NEXT: "main_loop"
+      },
+      {
+        speaker: "human",
+        input: {
+          variable: "reflection",
+          placeholder: "I learned..."
+        }
+      },
+      {
+        speaker: "bot",
+        content:
+          "Good to hear, {{=name}}! You can recall how you worked through this bug the next time you encounter another bug",
+        NEXT: "main_loop"
+      },
+      {
+        speaker: "bot",
+        content:
+          "If you need any help again, you can always call on lil' o me!",
         NEXT: "main_loop"
       }
     ]
